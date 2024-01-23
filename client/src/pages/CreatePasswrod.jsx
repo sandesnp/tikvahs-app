ac2dd2e58eac2360ce853804696586c0cea7af12
sandesnp <sthasandesnp@gmail.com>
2024-01-23 20:26:58 +1100

    removed unnecessary components from clients

--- client/src/pages/CreatePasswrod.jsx
+++ client/src/pages/CreatePasswrod.jsx
@@ -4,16 +4,12 @@ import { updateUser } from '../redux/userSlice';
 import { Navigate } from 'react-router-dom';
 
 import AnimationRevealPage from 'helpers/AnimationRevealPage.js';
-import { Container as ContainerBase } from 'components/misc/Layouts';
 import { ReactComponent as LoginIcon } from 'feather-icons/dist/icons/log-in.svg';
 import tw from 'twin.macro';
 import styled from 'styled-components';
 import illustration from 'images/login-illustration.svg';
 import logo from 'images/logo.svg';
 
-const Container = tw(
-  ContainerBase
-)`min-h-screen bg-primary-900 text-white font-medium flex justify-center -m-8`;
 const Content = tw.div`max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg flex justify-center flex-1`;
 const MainContainer = tw.div`lg:w-1/2 xl:w-5/12 p-6 sm:p-12`;
 const LogoLink = tw.a``;
